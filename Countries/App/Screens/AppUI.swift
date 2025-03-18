c60f08805b93789f37ad9978d3ff9a0543de2ce5
Mohammed Essam <eng.mohammedessam@gmail.com>
2025-03-18 17:03:55 +0200

    create new app

---
+++ Countries/App/Screens/AppUI.swift
@@ -0,0 +1,24 @@
+//
+//  AppUI.swift
+//  Countries
+//
+//  Created by Mohammed Essam on 18/03/2025.
+//
+
+import SwiftUI
+
+struct AppUI: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AppUI()
+}
---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Mohammed Essam on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppUI()
+        }
+    }
+}
